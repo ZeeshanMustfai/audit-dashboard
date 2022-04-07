1a7bb320617b9976635c36d7c580be6f0d062015
zeeshanmustfai <zeeshanmustfai22@gmail.com>
2022-04-06 18:03:25 -0700

    2nd commit of audit dashboard

--- src/components/requests/Requests.js
+++ src/components/requests/Requests.js
@@ -63,7 +63,7 @@ const Requests = () => {
   return (
     <>
       <Grid container spacing={2}>
-        <Grid item xs={12} md={8}>
+        <Grid item xs={12} md={12}>
           <Box
             sx={{
               display: "flex",
@@ -118,9 +118,9 @@ const Requests = () => {
           <CssBaseline />
           <TableContainer columns={columns} data={data} />
         </Grid>
-        <Grid item xs={12} md={4}>
+        {/* <Grid item xs={12} md={4}>
           <History />
-        </Grid>
+        </Grid> */}
       </Grid>
     </>
   );
